f8765165e7a19f4cfc1d5d3602692abf015c9e82
Sunny Vardhan <infinityat0@gmail.com>
2012-01-27 04:37:33 +0000

    Updating

--- web/SSAdmin Login/js/jquery.MySearchBox.js
+++
@@ -1,1 +0,0 @@
-$("#src_txt").live("blur", function(){	var default_value = $(this).attr("rel");	if ($(this).val() == ""){		$(this).val(default_value);	}}).live("focus", function(){	var default_value = $(this).attr("rel");	if ($(this).val() == default_value){		$(this).val("");	}});
